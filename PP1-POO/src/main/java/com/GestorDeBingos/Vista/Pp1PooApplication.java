2849dac87ea2b75f6ad60f030f5cc4f0a28d4204
DanielArce2901 <arcedaniel2901@gmail.com>
2023-09-28 12:36:38 -0600

    j

--- PP1-POO/src/main/java/com/GestorDeBingos/Vista/Pp1PooApplication.java
+++ PP1-POO/src/main/java/com/GestorDeBingos/Vista/Pp1PooApplication.java
@@ -1,13 +1,11 @@
 package com.GestorDeBingos.Vista;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Pp1PooApplication {
+public class MiAplicacionWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Pp1PooApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MiAplicacionWebApplication.class, args);
+    }
 }
