25817d8bd7cf646eec5cb720febd2d0d58f9a8fd
DanielArce2901 <arcedaniel2901@gmail.com>
2023-09-28 12:13:38 -0600

    de

---
+++ PP1-POO/src/main/java/com/GestorDeBingos/Vista/Pp1PooApplication.java
@@ -0,0 +1,13 @@
+package com.GestorDeBingos.Vista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp1PooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp1PooApplication.class, args);
+	}
+
+}
---
+++ PP1-POO/src/test/java/com/GestorDeBingos/Vista/Pp1PooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GestorDeBingos.Vista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp1PooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
